62534353c11fa9b9db9ae9c25ec1f18c70d4ba77
Sabelo Mcanyana <sabelom@gmail.com>
2019-04-26 15:53:35 +0000

    Java Script function to stop modal playing after close

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+
+// Carousel speed control
 $(document).ready(function(){
     $('.carousel').carousel({
       interval: 5000
     });
-})
+});
+
+// For stopping sound after closing video
+$(function(){
+  $('.modal').on('hidden.bs.modal', function (e) {
+    $iframe = $(this).find("iframe");
+    $iframe.attr("src", $iframe.attr("src"));
+  });
+});
+
